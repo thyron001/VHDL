60a07c918c194c51b4f43c860bc7bb4b27fa4710
Tyrone Novillo <tyminobra@outlook.es>
2024-06-10 01:58:29 -0500

    Practica 8 lista
    Se agrego codigo para controlar el color que se muestra mediante los switches de la Basys 3

--- Practica 7/vga.vhd
+++ Practica 7/vga.vhd
@@ -1,129 +1,159 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06/07/2024 09:27:24 AM
--- Design Name: 
--- Module Name: vga - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity vga is
-port (
-clk : in std_logic;
-hsync: out std_logic;
-vsync: out std_logic
-);
+    Port ( CLK_100M : in STD_LOGIC;
+           sw : in STD_LOGIC_VECTOR(0 to 11); -- Interruptores para controlar colores
+           vgaRed : out STD_LOGIC_VECTOR(3 downto 0);
+           vgaGreen : out STD_LOGIC_VECTOR(3 downto 0);
+           vgaBlue : out STD_LOGIC_VECTOR(3 downto 0);
+           Hsync : out STD_LOGIC;
+           Vsync : out STD_LOGIC );
 end vga;
 
 architecture Behavioral of vga is
-signal CLK_25M : std_logic;
-signal contador_25M : integer := 0;
 
-constant HD : integer := 640;    -- Ancho del área de visualización horizontal en píxeles
-constant HF : integer := 48;     -- Ancho del porch delantero horizontal en píxeles
-constant HB : integer := 16;     -- Ancho del porch trasero horizontal en píxeles
-constant HR : integer := 96;     -- Ancho del retroceso horizontal en píxeles
-constant HMAX : integer := HD + HF + HB + HR - 1; -- Valor máximo del contador horizontal = 799
+    -- Constantes de temporización VGA para 640x480 @ 60Hz
+    constant H_ACTIVA      : integer := 640;   -- Ancho de video activo
+    constant H_PORCH_DELANTERO : integer := 16;    -- Ancho del porch delantero
+    constant H_PULSO_SINCRONIA : integer := 96;    -- Ancho del pulso de sincronía
+    constant H_PORCH_TRASERO  : integer := 48;    -- Ancho del porch trasero
+    constant H_TOTAL       : integer := 800;   -- Ancho total de la línea
 
--- Definición de los parámetros verticales
-constant VD : integer := 480;    -- Longitud del área de visualización vertical en píxeles
-constant VF : integer := 10;     -- Longitud del porch delantero vertical en píxeles
-constant VB : integer := 33;     -- Longitud del porch trasero vertical en píxeles
-constant VR : integer := 2;      -- Longitud del retroceso vertical en píxeles
-constant VMAX : integer := VD + VF + VB + VR - 1; -- Valor máximo del contador vertical = 524
+    constant V_ACTIVA      : integer := 480;   -- Altura de video activo
+    constant V_PORCH_DELANTERO : integer := 10;    -- Altura del porch delantero
+    constant V_PULSO_SINCRONIA  : integer := 2;     -- Altura del pulso de sincronía
+    constant V_PORCH_TRASERO  : integer := 33;    -- Altura del porch trasero
+    constant V_TOTAL       : integer := 525;   -- Altura total del cuadro
 
--- Señales internas
-signal h_count_reg : integer range 0 to HMAX := 0;
-signal h_count_next : integer range 0 to HMAX;
-signal v_count_reg : integer range 0 to VMAX := 0;
-signal v_count_next : integer range 0 to VMAX;
+    -- Divisor de reloj para 25MHz
+    signal CLK_25M : STD_LOGIC := '0';
+    signal contador_div_reloj : integer := 0;
+    constant MAX_DIV_RELOJ : integer := 2;  -- Dividir 100MHz por 4 para obtener 25MHz
 
+    -- Declaración de señales
+    signal contador_h : integer range 0 to H_TOTAL-1 := 0;
+    signal contador_v : integer range 0 to V_TOTAL-1 := 0;
 
-
-signal hsync_int : STD_LOGIC;
-signal vsync_int : STD_LOGIC;
+    -- Tipos de estados para máquinas de estado
+    type estado_tipo is (ST0, ST1);
+    signal estado_actual_Hsync, estado_siguiente_Hsync : estado_tipo := ST0;
+    signal estado_actual_Vsync, estado_siguiente_Vsync : estado_tipo := ST0;
 
 begin
 
-reloj_25M: process(clk)
-begin
-    if rising_edge(clk) then
-        if contador_25M < 2 then
-            contador_25M <= contador_25M + 1;
-        else
-            CLK_25M <= not CLK_25M;
-            contador_25M <= 0;
-        end if;
-    end if;
-end process reloj_25M;
-
--------------------------------------------------------
-    -- Proceso para el contador vertical
-    process(clk)
+    -- Proceso divisor de reloj
+    process(CLK_100M)
     begin
-        v_count_reg <= v_count_next;
-        h_count_reg <= h_count_next;
---        v_sync_reg  <= v_sync_next;
---        h_sync_reg  <= h_sync_next;
-    end process;
-
-    -- Lógica secuencial para el contador horizontal
-    process(h_count_reg)
-    begin
-        if h_count_reg = HMAX  then
-            h_count_next <= 0;
-        else
-            h_count_next <= h_count_reg + 1;
-        end if;
-    end process;
-    
-    ----------------------------------------------------------
-    
-    -- Lógica secuencial para el contador vertical
-    process(h_count_reg, v_count_reg)
-    begin
-        if h_count_reg = HMAX then
-            if v_count_reg = VMAX then
-                v_count_next <= 0;
+        if rising_edge(CLK_100M) then
+            if contador_div_reloj = MAX_DIV_RELOJ-1 then
+                CLK_25M <= not CLK_25M;
+                contador_div_reloj <= 0;
             else
-                v_count_next <= v_count_reg + 1;
+                contador_div_reloj <= contador_div_reloj + 1;
             end if;
-        else
-            v_count_next <= v_count_reg;
         end if;
     end process;
----------------------------------------------------------------------
-hsync <= '0' when (h_count_reg >= (HD + HB) and h_count_reg <= (HD + HB + HR - 1)) else '1';
-vsync <= '0' when (v_count_reg >= (VD + VB) and v_count_reg <= (VD + VB + VR - 1)) else '1';
 
--- h_sync_next asserted within the horizontal retrace area
-h_sync_next <= (h_count_reg >= (HD+HB) && h_count_reg <= (HD+HB+HR-1));
-    
--- v_sync_next asserted within the vertical retrace area
-v_sync_next <= (v_count_reg >= (VD+VB) && v_count_reg <= (VD+VB+VR-1));
+    -- Proceso de sincronización VGA
+    process(CLK_25M)
+    begin
+        if rising_edge(CLK_25M) then
+            -- Contador horizontal
+            if contador_h = H_TOTAL-1 then
+                contador_h <= 0;
+                -- Contador vertical
+                if contador_v = V_TOTAL-1 then
+                    contador_v <= 0;
+                else
+                    contador_v <= contador_v + 1;
+                end if;
+            else
+                contador_h <= contador_h + 1;
+            end if;
+        end if;
+    end process;
+
+    -- Máquina de estado para Hsync
+    process(CLK_25M)
+    begin
+        if rising_edge(CLK_25M) then
+            estado_actual_Hsync <= estado_siguiente_Hsync;
+        end if;
+    end process;
+
+    process(estado_actual_Hsync, contador_h)
+    begin
+        case estado_actual_Hsync is
+            when ST0 =>
+                if (contador_h >= H_ACTIVA + H_PORCH_DELANTERO and contador_h < H_ACTIVA + H_PORCH_DELANTERO + H_PULSO_SINCRONIA) then
+                    Hsync <= '0';
+                    estado_siguiente_Hsync <= ST1;
+                else
+                    Hsync <= '1';
+                    estado_siguiente_Hsync <= ST0;
+                end if;
+            when ST1 =>
+                if (contador_h >= H_ACTIVA + H_PORCH_DELANTERO + H_PULSO_SINCRONIA) then
+                    Hsync <= '1';
+                    estado_siguiente_Hsync <= ST0;
+                else
+                    Hsync <= '0';
+                    estado_siguiente_Hsync <= ST1;
+                end if;
+            when others =>
+                Hsync <= '1';
+                estado_siguiente_Hsync <= ST0;
+        end case;
+    end process;
+
+    -- Máquina de estado para Vsync
+    process(CLK_25M)
+    begin
+        if rising_edge(CLK_25M) then
+            estado_actual_Vsync <= estado_siguiente_Vsync;
+        end if;
+    end process;
+
+    process(estado_actual_Vsync, contador_v)
+    begin
+        case estado_actual_Vsync is
+            when ST0 =>
+                if (contador_v >= V_ACTIVA + V_PORCH_DELANTERO and contador_v < V_ACTIVA + V_PORCH_DELANTERO + V_PULSO_SINCRONIA) then
+                    Vsync <= '0';
+                    estado_siguiente_Vsync <= ST1;
+                else
+                    Vsync <= '1';
+                    estado_siguiente_Vsync <= ST0;
+                end if;
+            when ST1 =>
+                if (contador_v >= V_ACTIVA + V_PORCH_DELANTERO + V_PULSO_SINCRONIA) then
+                    Vsync <= '1';
+                    estado_siguiente_Vsync <= ST0;
+                else
+                    Vsync <= '0';
+                    estado_siguiente_Vsync <= ST1;
+                end if;
+            when others =>
+                Vsync <= '1';
+                estado_siguiente_Vsync <= ST0;
+        end case;
+    end process;
+
+    -- Establecer colores (Pantalla controlada por interruptores)
+    process(CLK_25M)
+    begin
+        if rising_edge(CLK_25M) then
+            if contador_h < H_ACTIVA and contador_v < V_ACTIVA then
+                vgaRed <= sw(0 to 3);
+                vgaGreen <= sw(4 to 7);
+                vgaBlue <= sw(8 to 11);
+            else
+                vgaRed <= "0000";
+                vgaGreen <= "0000";
+                vgaBlue <= "0000";
+            end if;
+        end if;
+    end process;
 
 end Behavioral;
